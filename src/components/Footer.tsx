038bc288d1ce91f9e39540bd76e7b94d6e2eef3d
JunYoungKr <kop981020@naver.com>
2025-05-13 23:00:42 +0900

    feat: Header, Footer 구현 / Layout 삭제

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+  return (
+    <footer
+      className="text-lg text-black"
+      style={{
+        height: "80px",
+        padding: "0 24px",
+        borderTop: "1px solid #111",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      © 2020 김준영
+    </footer>
+  );
+};
+
+export default Footer;
